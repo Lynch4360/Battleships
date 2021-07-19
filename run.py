9cf09edd98f5490cc3189d9b4158d6f6d35666d3
Lynch4360 <lynch4360@gmail.com>
2021-07-19 22:10:11 +0000

    Add user input and try and except for input

--- run.py
+++ run.py
@@ -1,4 +1,44 @@
-print("            Battleships       ")
-print("     0  1  2  3  4  5  6  7  8  9")
-for x in range(10):
-    print(x, " ", " _ "*10)
+def get_shot():
+
+    ok = "n"
+    while ok == "n":
+        try:
+            shot = input("please enter your guess")
+            shot = int(shot)
+            if shot < 0 or shot > 99:
+                print("incorrect number, please try again")
+            else:
+                ok = "y"
+                break
+        except:
+            print("please enter again")
+    return shot
+
+
+def show_board(hit, miss, comp):
+    print("            Battleships       ")
+    print("     0  1  2  3  4  5  6  7  8  9")
+
+    place = 0
+    for x in range(10):
+        row = ""
+        for y in range(10):
+            ch = " _ "
+            if place in miss:
+                ch = " x "
+            elif place in hit:
+                ch = " o "
+            elif place in comp:
+                ch = " O "
+
+            row = row + ch
+            place = place + 1
+        print(x, " ", row)
+
+
+hit = [21, 22, 23]
+miss = [20, 24, 12, 13]
+comp = [23]
+
+shot = get_shot()
+show_board(hit, miss, comp)
